02d23cbf2a7166332877f4ecb0f5879b32e23bdd
Alois Heloir <alois.heloir@live.fr>
2022-11-16 11:57:54 +0100

    Add First version of singleton

--- hivers/src/main/java/org/mti/hivers/Singleton.java
+++ hivers/src/main/java/org/mti/hivers/Singleton.java
@@ -1,8 +1,19 @@
 package org.mti.hivers;
 
-public class Singleton {
+import java.util.HashMap;
+import java.util.Map;
 
-    static public String helloWorld() {
-        return "Hello World";
+public class Singleton<T> {
+    static private Map<Class<Object>, Object> createdSingletons = new HashMap<>();
+
+    Singleton(Class<T> classObject, T classInstance) {
+        if (Singleton.createdSingletons.containsKey(classObject)) {
+            return;
+        }
+        Singleton.createdSingletons.put((Class<Object>) classObject, classInstance);
+    }
+
+    public T instanceOf(Class<T> classObject) {
+        return (T) Singleton.createdSingletons.get(classObject);
     }
 }
--- hivers/src/test/java/org/mti/hivers/SingletonTest.java
+++ hivers/src/test/java/org/mti/hivers/SingletonTest.java
@@ -2,13 +2,39 @@ package org.mti.hivers;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.Signature;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SingletonTest {
 
-    @Test
-    void helloWorld() {
+    public static class PingService {
+        public String ping() { return "Ping";}
+    }
 
-        assertSame(Singleton.helloWorld(), "Hello World");
+    @Test
+    void shouldCreateSingleton() {
+        var singleton = new Singleton<>(PingService.class, new PingService());
+        assertNotNull(singleton);
+    }
+
+    @Test
+    void shouldBeAbleToCreateAndRetrieveSingleton() {
+        var singleton = new Singleton<>(PingService.class, new PingService());
+        assertNotNull(singleton.instanceOf(PingService.class));
+    }
+
+    @Test
+    void shouldBeAbleToCreateAndRetrieveAndUseIt() {
+        var singleton = new Singleton<>(PingService.class, new PingService());
+        assertEquals(singleton.instanceOf(PingService.class).ping(), "Ping");
+    }
+
+    @Test
+    void shouldntCreateMultipleInstance() {
+        var singleton1 = new Singleton<>(PingService.class, new PingService());
+        var singleton2 = new Singleton<>(PingService.class, new PingService());
+
+        assert (singleton1.instanceOf(PingService.class).equals(singleton2.instanceOf(PingService.class)));
     }
 }
