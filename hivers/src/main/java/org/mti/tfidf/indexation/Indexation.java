e36ac76d152c74eca73677098ef5912c06689566
Théo Casner <theo.casner@epita.fr>
2022-11-23 11:46:20 +0100

    add: move to resources + add

--- hivers/src/main/java/org/mti/tfidf/indexation/Indexation.java
+++ hivers/src/main/java/org/mti/tfidf/indexation/Indexation.java
@@ -3,22 +3,56 @@ package org.mti.tfidf.indexation;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.net.MalformedURLException;
+import java.io.*;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Indexation {
     public static void main(String[] args) {
         Indexation ind = new Indexation();
-        ind.htmlToRawText();
+        var text = ind.htmlToRawText("https://example.com");
+        var tokens = ind.textToTokens(text);
+        System.out.println(tokens.size());
+        var cleanedTokens = ind.removeStopWords(tokens);
+        System.out.println(cleanedTokens);
     }
 
-    public String htmlToRawText() {
+    public String htmlToRawText(String url) {
         try {
-            Document doc = Jsoup.parse(new URL("https://example.com"), 10000);
+            Document doc = Jsoup.parse(new URL(url), 10000);
             doc.outputSettings().prettyPrint(false);
             return doc.body().text();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<String> textToTokens(String text) {
+        return List.of(text.toLowerCase().split("\\s+"));
+    }
+
+    public List<String> removeStopWords(List<String> tokens) {
+        Set<String> stopWords = this.getStopWordsFromFile();
+
+        return tokens.stream().filter((token) -> !stopWords.contains(token)).toList();
+    }
+
+    public List<String> 
+
+    private Set<String> getStopWordsFromFile() {
+        Set<String> stopWords = new HashSet<>();
+
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("stop_words.txt");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stopWords.add(line);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return stopWords;
     }
 }
