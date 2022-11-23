7929d80d01f852d5bfeb9e285822cddd6c2bd872
Théo Casner <theo.casner@epita.fr>
2022-11-23 11:58:52 +0100

    add: synonyms replacement

--- hivers/src/main/java/org/mti/tfidf/indexation/Indexation.java
+++ hivers/src/main/java/org/mti/tfidf/indexation/Indexation.java
@@ -5,18 +5,17 @@ import org.jsoup.nodes.Document;
 
 import java.io.*;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Indexation {
     public static void main(String[] args) {
         Indexation ind = new Indexation();
         var text = ind.htmlToRawText("https://example.com");
         var tokens = ind.textToTokens(text);
-        System.out.println(tokens.size());
         var cleanedTokens = ind.removeStopWords(tokens);
         System.out.println(cleanedTokens);
+        var tokensReplacedWithSynonyms = ind.replaceSynonyms(cleanedTokens);
+        System.out.println(tokensReplacedWithSynonyms);
     }
 
     public String htmlToRawText(String url) {
@@ -39,7 +38,15 @@ public class Indexation {
         return tokens.stream().filter((token) -> !stopWords.contains(token)).toList();
     }
 
-    public List<String> 
+    public List<String> replaceSynonyms(List<String> tokens) {
+        var synonyms = this.getSynonyms();
+
+        return tokens.stream().map((token) -> {
+            var synonym = synonyms.get(token);
+            if (synonym == null) return token;
+            return synonym;
+        }).toList();
+    }
 
     private Set<String> getStopWordsFromFile() {
         Set<String> stopWords = new HashSet<>();
@@ -54,5 +61,25 @@ public class Indexation {
             throw new RuntimeException(e);
         }
         return stopWords;
+    }
+
+    private Map<String, String> getSynonyms() {
+        Map<String, String> synonyms = new HashMap<>();
+
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("synonyms.txt");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                var words = line.split(" ");
+                var mainSynonym = words[0];
+
+                for (var word : words) {
+                    synonyms.put(word, mainSynonym);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return synonyms;
     }
 }
